f7cb6ca44c1b3d0c6f8754e90b02b0c8b10f89f9
SmugTheKiler <emirgamaleev2009@gmail.com>
2022-11-19 18:22:16 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+/* function search() {
+  let output = 'valid';
+  var input = document.getElementById('city-input').value;
+
+  fetch(apiUrl).then(response => {
+    return response.json();
+  }).then(data => {
+    // Work with JSON data here
+    console.log(data);
+  }).catch(err => {
+    // Do something for an error here
+  });
+
+  document.getElementById("output").innerHTML = input;
+    }; */
